e99bc98d7075c54c206bc2ba4661386b6c8e6938
Kazuki Sawada <kazuki@6715.jp>
2020-01-29 13:41:24 +0900

    add fish

---
+++ fish/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eux
+
+FISH_VERSION=3.0.2
+
+apk add gcc g++ make cmake musl-dev ncurses-dev ncurses-static
+
+ln /usr/lib/libncursesw.a /usr/lib/libcurses.a
+
+cd /
+wget https://github.com/fish-shell/fish-shell/releases/download/${FISH_VERSION}/fish-${FISH_VERSION}.tar.gz -O - | tar zxvf -
+cd fish-${FISH_VERSION}
+cmake -DCMAKE_INSTALL_PREFIX=/build/artifacts/fish-${FISH_VERSION} -DCMAKE_C_FLAGS="-Ofast" -DCMAKE_CXX_FLAGS="-Ofast" -DCMAKE_EXE_LINKER_FLAGS="-static -no-pie -s" .
+make -j8 install
+
+cd /build/artifacts
+tar zcvf fish-${FISH_VERSION}.tar.gz fish-${FISH_VERSION}
