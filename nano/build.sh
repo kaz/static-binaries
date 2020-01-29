44b305de127c82244f4ae46578160ce57695573e
Kazuki Sawada <kazuki@6715.jp>
2020-01-29 11:43:55 +0900

    add nano

---
+++ nano/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -eux
+
+FILE_VERSION=5.38
+NANO_VERSION=4.7
+
+apk add gcc make groff linux-headers musl-dev ncurses-dev ncurses-static zlib-dev zlib-static
+
+cd /
+wget ftp://ftp.astron.com/pub/file/file-${FILE_VERSION}.tar.gz -O - | tar zxvf -
+cd file-${FILE_VERSION}
+./configure --prefix=/usr CFLAGS="-Ofast" --enable-static
+make -j8 install
+
+cd /
+wget https://www.nano-editor.org/dist/v4/nano-${NANO_VERSION}.tar.gz -O - | tar zxvf -
+cd nano-${NANO_VERSION}
+./configure --prefix=/build/artifacts/nano-${NANO_VERSION} CFLAGS="-Ofast" LDFLAGS="-static -no-pie -s" LIBS="-lz"
+make -j8 install
+
+cd /build/artifacts
+tar zcvf nano-${NANO_VERSION}.tar.gz nano-${NANO_VERSION}
