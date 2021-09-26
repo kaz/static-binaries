700ebef6c9512dca96be3d448f97bc6503c2396d
eggplants <w10776e8w@yahoo.co.jp>
2021-09-26 09:25:59 +0900

    fix: nano/build.sh was shellchecked

--- nano/build.sh
+++ nano/build.sh
@@ -1,23 +1,27 @@
 #!/bin/sh -eux
 
-FILE_VERSION=5.39
-NANO_VERSION=5.3
+readonly FILE_VERSION NANO_VERSION ARTIFACTS_DIR
 
-ARTIFACTS_DIR=$(realpath $(dirname ${0})/artifacts)
+FILE_VERSION=5.40
+NANO_VERSION=5.8
 
-apk add gcc make groff linux-headers musl-dev ncurses-dev ncurses-static zlib-dev zlib-static
+ARTIFACTS_DIR="$(realpath ./artifacts)"
+
+apk add gcc groff linux-headers make musl-dev \
+        ncurses-dev ncurses-static zlib-dev zlib-static
 
 cd /tmp
-wget http://ftp.astron.com/pub/file/file-${FILE_VERSION}.tar.gz -O - | tar zxvf -
-cd file-${FILE_VERSION}
+wget "http://ftp.astron.com/pub/file/file-${FILE_VERSION}.tar.gz" -O - | tar zxvf -
+cd "file-${FILE_VERSION}"
 ./configure --prefix=/usr CFLAGS="-Ofast" --enable-static
 make -j8 install
 
 cd /tmp
-wget https://www.nano-editor.org/dist/v${NANO_VERSION%%.*}/nano-${NANO_VERSION}.tar.gz -O - | tar zxvf -
-cd nano-${NANO_VERSION}
-./configure --prefix=${ARTIFACTS_DIR}/nano-${NANO_VERSION} CFLAGS="-Ofast" LDFLAGS="-static -no-pie -s" LIBS="-lz"
+wget -O- "https://www.nano-editor.org/dist/v${NANO_VERSION%%.*}/nano-${NANO_VERSION}.tar.gz" \
+     | tar zxvf -
+cd "nano-${NANO_VERSION}"
+./configure --prefix="${ARTIFACTS_DIR}/nano-${NANO_VERSION}" CFLAGS="-Ofast" LDFLAGS="-static -no-pie -s" LIBS="-lz"
 make -j8 install
 
-cd ${ARTIFACTS_DIR}
-tar zcvf nano-${NANO_VERSION}.tar.gz nano-${NANO_VERSION}
+cd "${ARTIFACTS_DIR}"
+tar zcvf "nano-${NANO_VERSION}.tar.gz" "nano-${NANO_VERSION}"
